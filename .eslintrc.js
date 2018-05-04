3dc09135f60d8297daf15fce6196d29af3d50093
宋鹏 <songpeng02@meituan.com>
2018-05-04 14:14:54 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+//https://eslint.org/docs/user-guide/configuring
+module.exports = {
+  root: true,
+  extends: [
+    'eslint-config-mfe/eslintrc.node.js',
+    'eslint-config-mfe/eslintrc.typescript-node.js'
+  ],
+  rules: {
+    indent: [2, 2, { SwitchCase: 1 }],
+    'no-console': 0
+  }
+};
